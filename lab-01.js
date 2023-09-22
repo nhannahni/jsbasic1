c78626c095e35439be6039b9856edd54f52c01a0
nhannahni <tranhuynhtrinhan@gmail.com>
2023-09-22 19:29:55 +0700

    nahNi

--- lab-01.js
+++ lab-01.js
@@ -1,3 +1,4 @@
+console.log("=====================")
 //Bai 1a,b
 console.log("hi I'm nahNi")
 //Bai 1d
@@ -26,6 +27,44 @@ console.log("")
 console.log('|`  |  /`   |   | |`  | |')
 console.log('| ` | /- `  |-- | | ` | |')
 console.log('|  `|/    ` |   | |  `| |')
+// Bai 2 a,b,c 
+let a = 10
+let b = 10
+console.log(a)
+// Bai 2 e,f
+const pi = 3.14
+let ten = "iPhone 14"
+let hangSanXuat = "Apple"
+let mauSac = "Trang"
+let gia = "1000 USD"
+console.log(pi)
+console.log(ten)
+console.log(hangSanXuat)
+console.log(mauSac)
+console.log(gia)
+// Bai 3a
+console.log(a,b)
+console.log(a+b)
+console.log(hangSanXuat+" "+ten)
+let x=3
+let y=4
+let z=6
+console.log('============')
+let dienThoai={
+    ten: 'iPhone 14',
+    hangSanXuat:'Apple',
+    mauSac: 'trang',
+    gia:"1000USD",
+    trongLuong: '200g'
+}
+console.log(dienThoai)
+console.log('============')
+let nahNi={
+    namSinh: 1997,
+    canNang: '60kg',
+    chieuCao: '169cm',
 
-let nahNi = 17
+}
+nahNi.soDienThoai ="0388468949"
+nahNi.chieuCao = "20cm"
 console.log(nahNi)
